730efe4a6e881ac439b3526bfaabcc33642f21e0
Alvi123787 <rebalalvi123@gmail.com>
2026-06-03 06:09:42 -0700

    update serverall  commit

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ const PORT = process.env.PORT || 5000
 if (process.env.NODE_ENV !== 'production') {
   import('./utils/db.js').then(({ default: connectDB }) => {
     connectDB().then(() => {
-      app.listen(PORT, () => {
+      app.listen(PORT, '0.0.0.0', () => {
         console.log(`🚀 Server running on http://localhost:${PORT}`)
       })
     })
